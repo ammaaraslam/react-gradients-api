286f1e084cf425d3c5603c465d77235e4d3ce626
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-08-13 15:18:46 +0530

    configured and added firebase to project

--- database/firebase.js
+++ database/firebase.js
@@ -1,1 +1,16 @@
 const firebase = require("firebase");
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig); //initialize firebase app
+module.exports = { firebase }; //export the app
